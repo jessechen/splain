a1cc1d77e9a2be3ceaa09661e24078472dcc3c5a
Jesse Chen <jesse.z.chen@gmail.com>
2019-02-22 16:42:50 -0500

    Clean up duplication in recalculateCurve

--- splain.js
+++ splain.js
@@ -1,26 +1,26 @@
-const quadraticMarkup = "<path id=\"p\" d=\"M30,30Q90 30 60 60\" stroke=\"#00f\" stroke-width=\"2\" fill=\"transparent\"></path>\n" +
-    "            <circle id=\"c1\" class=\"control\" cx=\"30\" cy=\"30\" r=\"4\" fill=\"#f00\"></circle>\n" +
-    "            <circle id=\"c2\" class=\"control\" cx=\"90\" cy=\"30\" r=\"4\" fill=\"#f00\"></circle>\n" +
-    "            <circle id=\"c3\" class=\"control\" cx=\"60\" cy=\"60\" r=\"4\" fill=\"#f00\"></circle>";
-const cubicMarkup = "<line id=\"l1\" x1=\"30\" y1=\"60\" x2=\"20\" y2=\"30\" stroke=\"#88f\"></line>\n" +
-    "                <line id=\"l2\" x1=\"70\" y1=\"30\" x2=\"60\" y2=\"60\" stroke=\"#88f\"></line>\n" +
-    "                <path id=\"p\" d=\"M30,60C20 30,70 30,60 60\" stroke=\"#00f\" stroke-width=\"2\" fill=\"transparent\"></path>\n" +
-    "                <circle id=\"c1\" class=\"control\" cx=\"30\" cy=\"60\" r=\"4\" fill=\"#f00\"></circle>\n" +
-    "                <circle id=\"c2\" class=\"control\" cx=\"20\" cy=\"30\" r=\"4\" fill=\"#f00\"></circle>\n" +
-    "                <circle id=\"c3\" class=\"control\" cx=\"70\" cy=\"30\" r=\"4\" fill=\"#f00\"></circle>\n" +
-    "                <circle id=\"c4\" class=\"control\" cx=\"60\" cy=\"60\" r=\"4\" fill=\"#f00\"></circle>";
-const continuousMarkup = "<line id=\"l1\" x1=\"20\" y1=\"60\" x2=\"10\" y2=\"30\" stroke=\"#88f\"></line>\n" +
-    "            <line id=\"l2\" x1=\"60\" y1=\"30\" x2=\"50\" y2=\"60\" stroke=\"#88f\"></line>\n" +
-    "            <line id=\"l3\" x1=\"50\" y1=\"60\" x2=\"40\" y2=\"90\" stroke=\"#484\"></line>\n" +
-    "            <line id=\"l4\" x1=\"90\" y1=\"30\" x2=\"80\" y2=\"60\" stroke=\"#88f\"></line>\n" +
-    "            <path id=\"p\" d=\"M20,60C10 30,60 30,50 60S90 30,80 60\" stroke=\"#00f\" stroke-width=\"2\" fill=\"transparent\"></path>\n" +
-    "            <circle id=\"c1\" class=\"control\" cx=\"20\" cy=\"60\" r=\"4\" fill=\"#f00\"></circle>\n" +
-    "            <circle id=\"c2\" class=\"control\" cx=\"10\" cy=\"30\" r=\"4\" fill=\"#f00\"></circle>\n" +
-    "            <circle id=\"c3\" class=\"control\" cx=\"60\" cy=\"30\" r=\"4\" fill=\"#f00\"></circle>\n" +
-    "            <circle id=\"c4\" class=\"control\" cx=\"50\" cy=\"60\" r=\"4\" fill=\"#f00\"></circle>\n" +
-    "            <circle id=\"c3b\" cx=\"40\" cy=\"90\" r=\"4\" fill=\"#080\"></circle>\n" +
-    "            <circle id=\"c5\" class=\"control\" cx=\"90\" cy=\"30\" r=\"4\" fill=\"#f00\"></circle>\n" +
-    "            <circle id=\"c6\" class=\"control\" cx=\"80\" cy=\"60\" r=\"4\" fill=\"#f00\"></circle>";
+const quadraticMarkup = '<path id=\'p\' d=\'M30,30Q90 30 60 60\' stroke=\'#00f\' stroke-width=\'2\' fill=\'transparent\'></path>\n' +
+    '            <circle id=\'c1\' class=\'control\' cx=\'30\' cy=\'30\' r=\'4\' fill=\'#f00\'></circle>\n' +
+    '            <circle id=\'c2\' class=\'control\' cx=\'90\' cy=\'30\' r=\'4\' fill=\'#f00\'></circle>\n' +
+    '            <circle id=\'c3\' class=\'control\' cx=\'60\' cy=\'60\' r=\'4\' fill=\'#f00\'></circle>';
+const cubicMarkup = '<line id=\'l1\' x1=\'30\' y1=\'60\' x2=\'20\' y2=\'30\' stroke=\'#88f\'></line>\n' +
+    '                <line id=\'l2\' x1=\'70\' y1=\'30\' x2=\'60\' y2=\'60\' stroke=\'#88f\'></line>\n' +
+    '                <path id=\'p\' d=\'M30,60C20 30,70 30,60 60\' stroke=\'#00f\' stroke-width=\'2\' fill=\'transparent\'></path>\n' +
+    '                <circle id=\'c1\' class=\'control\' cx=\'30\' cy=\'60\' r=\'4\' fill=\'#f00\'></circle>\n' +
+    '                <circle id=\'c2\' class=\'control\' cx=\'20\' cy=\'30\' r=\'4\' fill=\'#f00\'></circle>\n' +
+    '                <circle id=\'c3\' class=\'control\' cx=\'70\' cy=\'30\' r=\'4\' fill=\'#f00\'></circle>\n' +
+    '                <circle id=\'c4\' class=\'control\' cx=\'60\' cy=\'60\' r=\'4\' fill=\'#f00\'></circle>';
+const continuousMarkup = '<line id=\'l1\' x1=\'20\' y1=\'60\' x2=\'10\' y2=\'30\' stroke=\'#88f\'></line>\n' +
+    '            <line id=\'l2\' x1=\'60\' y1=\'30\' x2=\'50\' y2=\'60\' stroke=\'#88f\'></line>\n' +
+    '            <line id=\'l3\' x1=\'50\' y1=\'60\' x2=\'40\' y2=\'90\' stroke=\'#484\'></line>\n' +
+    '            <line id=\'l4\' x1=\'90\' y1=\'30\' x2=\'80\' y2=\'60\' stroke=\'#88f\'></line>\n' +
+    '            <path id=\'p\' d=\'M20,60C10 30,60 30,50 60S90 30,80 60\' stroke=\'#00f\' stroke-width=\'2\' fill=\'transparent\'></path>\n' +
+    '            <circle id=\'c1\' class=\'control\' cx=\'20\' cy=\'60\' r=\'4\' fill=\'#f00\'></circle>\n' +
+    '            <circle id=\'c2\' class=\'control\' cx=\'10\' cy=\'30\' r=\'4\' fill=\'#f00\'></circle>\n' +
+    '            <circle id=\'c3\' class=\'control\' cx=\'60\' cy=\'30\' r=\'4\' fill=\'#f00\'></circle>\n' +
+    '            <circle id=\'c4\' class=\'control\' cx=\'50\' cy=\'60\' r=\'4\' fill=\'#f00\'></circle>\n' +
+    '            <circle id=\'c3b\' cx=\'40\' cy=\'90\' r=\'4\' fill=\'#080\'></circle>\n' +
+    '            <circle id=\'c5\' class=\'control\' cx=\'90\' cy=\'30\' r=\'4\' fill=\'#f00\'></circle>\n' +
+    '            <circle id=\'c6\' class=\'control\' cx=\'80\' cy=\'60\' r=\'4\' fill=\'#f00\'></circle>';
 const markupRegistry = [quadraticMarkup, cubicMarkup, continuousMarkup];
 
 let dragObject = null;
@@ -34,55 +34,46 @@ const toCanvasCoords = function(e) {
     return [Math.round(result.x), Math.round(result.y)];
 };
 
+const getCoords = function(elementId) {
+    const x = parseInt(document.getElementById(elementId).getAttribute('cx'), 10);
+    const y = parseInt(document.getElementById(elementId).getAttribute('cy'), 10);
+    return {x: x, y: y};
+};
+
+const setLineEndpoints = function(lineId, point1, point2) {
+    const line = document.getElementById(lineId);
+    line.setAttribute('x1', point1.x);
+    line.setAttribute('y1', point1.y);
+    line.setAttribute('x2', point2.x);
+    line.setAttribute('y2', point2.y);
+};
+
 const recalculateCurve = function() {
-    const x1 = parseInt(document.getElementById('c1').getAttribute('cx'), 10);
-    const y1 = parseInt(document.getElementById('c1').getAttribute('cy'), 10);
-    const x2 = parseInt(document.getElementById('c2').getAttribute('cx'), 10);
-    const y2 = parseInt(document.getElementById('c2').getAttribute('cy'), 10);
-    const x3 = parseInt(document.getElementById('c3').getAttribute('cx'), 10);
-    const y3 = parseInt(document.getElementById('c3').getAttribute('cy'), 10);
+    const point1 = getCoords('c1');
+    const point2 = getCoords('c2');
+    const point3 = getCoords('c3');
     let curveString;
     if (!document.getElementById('c4')) {
-        curveString = `M${x1},${y1}Q${x2} ${y2} ${x3} ${y3}`;
+        curveString = `M${point1.x},${point1.y}Q${point2.x} ${point2.y},${point3.x} ${point3.y}`;
     } else {
-        const x4 = parseInt(document.getElementById('c4').getAttribute('cx'), 10);
-        const y4 = parseInt(document.getElementById('c4').getAttribute('cy'), 10);
-        const line1 = document.getElementById("l1");
-        line1.setAttribute("x1", x1);
-        line1.setAttribute("y1", y1);
-        line1.setAttribute("x2", x2);
-        line1.setAttribute("y2", y2);
-        const line2 = document.getElementById("l2");
-        line2.setAttribute("x1", x3);
-        line2.setAttribute("y1", y3);
-        line2.setAttribute("x2", x4);
-        line2.setAttribute("y2", y4);
+        const point4 = getCoords('c4');
+        setLineEndpoints('l1', point1, point2);
+        setLineEndpoints('l2', point3, point4);
         if (!document.getElementById('c5')) {
-            curveString = `M${x1},${y1}C${x2} ${y2},${x3} ${y3},${x4} ${y4}`;
+            curveString = `M${point1.x},${point1.y}C${point2.x} ${point2.y},${point3.x} ${point3.y},${point4.x} ${point4.y}`;
         } else {
-            const x5 = parseInt(document.getElementById('c5').getAttribute('cx'), 10);
-            const y5 = parseInt(document.getElementById('c5').getAttribute('cy'), 10);
-            const x6 = parseInt(document.getElementById('c6').getAttribute('cx'), 10);
-            const y6 = parseInt(document.getElementById('c6').getAttribute('cy'), 10);
-            const x3b = x4 + x4 - x3;
-            const y3b = y4 + y4 - y3;
-            const c3b = document.getElementById("c3b");
-            c3b.setAttribute("cx", x3b);
-            c3b.setAttribute("cy", y3b);
-            const line3 = document.getElementById("l3");
-            line3.setAttribute("x1", x3b);
-            line3.setAttribute("y1", y3b);
-            line3.setAttribute("x2", x4);
-            line3.setAttribute("y2", y4);
-            const line4 = document.getElementById("l4");
-            line4.setAttribute("x1", x5);
-            line4.setAttribute("y1", y5);
-            line4.setAttribute("x2", x6);
-            line4.setAttribute("y2", y6);
-            curveString = `M${x1},${y1}C${x2} ${y2},${x3} ${y3},${x4} ${y4}S${x5} ${y5},${x6} ${y6}`;
+            const point5 = getCoords('c5');
+            const point6 = getCoords('c6');
+            const point3b = {x: 2 * point4.x - point3.x, y: 2 * point4.y - point3.y};
+            const c3b = document.getElementById('c3b');
+            c3b.setAttribute('cx', point3b.x);
+            c3b.setAttribute('cy', point3b.y);
+            setLineEndpoints('l3', point3b, point4);
+            setLineEndpoints('l4', point5, point6);
+            curveString = `M${point1.x},${point1.y}C${point2.x} ${point2.y},${point3.x} ${point3.y},${point4.x} ${point4.y}S${point5.x} ${point5.y},${point6.x} ${point6.y}`;
         }
     }
-    const path = document.getElementById("p");
+    const path = document.getElementById('p');
     path.setAttribute('d', curveString);
     updateUi();
 };
